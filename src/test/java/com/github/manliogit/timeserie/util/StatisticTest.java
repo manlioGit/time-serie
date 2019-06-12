66254e2926d659edd32f48a3494be27426bd8960
manlio <manlio@HP-cs2xxx>
2019-06-12 19:54:44 +0200

    end documentation draft

--- src/test/java/com/github/manliogit/timeserie/util/StatisticTest.java
+++ src/test/java/com/github/manliogit/timeserie/util/StatisticTest.java
@@ -13,22 +13,22 @@ import org.junit.Test;
 public class StatisticTest {
 
 	@Test
-	public void testMean() throws Exception {
+	public void testMean() {
 		assertThat(mean(10.,12.,14.,15.,17.,18.,18.,24.), is(16.));
 	}
 	
 	@Test
-	public void variance() throws Exception {
+	public void variance() {
 		assertThat(var(9., 2., 4., 5., 7., 3.), closeTo(5.6, 0.1));
 	}
 	
 	@Test
-	public void standardDeviation() throws Exception {
+	public void standardDeviation() {
 		assertThat(sd(9., 2., 4., 5., 7., 3.), closeTo(2.36, 0.1));
 	}
 	
 	@Test
-	public void summation() throws Exception {
+	public void summation() {
 		assertThat(sum(1.,2.,3.,4.,5.), is(15.));
 	}
 }
